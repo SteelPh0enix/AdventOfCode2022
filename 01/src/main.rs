60161fd4fb5409ac41f389363d0737d7ddbf8c99
SteelPh0enix <steelphoenix2137@gmail.com>
2022-12-01 18:54:15 +0100

    simplified fold to sum

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -9,14 +9,14 @@ fn main() {
         .map(|line| line.parse().unwrap_or(ELVES_SEPARATOR))
         .collect::<Vec<i64>>()
         .split(|&weight| weight == ELVES_SEPARATOR)
-        .map(|calories| calories.iter().fold(0u64, |acc, &x| acc + (x as u64)))
-        .collect::<Vec<u64>>();
+        .map(|calories| calories.iter().sum())
+        .collect::<Vec<i64>>();
 
     elves.sort_unstable();
 
     let most_calories = elves.last().unwrap();
     println!("Elf carrying the most Calories carries {most_calories} Calories");
 
-    let top_3_total_calories: u64 = elves.iter().rev().take(3).sum();
+    let top_3_total_calories: i64 = elves.iter().rev().take(3).sum();
     println!("Top 3 elves have {top_3_total_calories} Calories");
 }
